981a24f141dbe1e363dad7b7dd0d81b78b80d415
JRafael <rafaelmatiashue@gmail.com>
2022-08-16 10:49:15 -0300

    atualizando inicio

--- src/main/java/com/consultorioapp/ifsertaope/controller/ConsultorioController.java
+++ src/main/java/com/consultorioapp/ifsertaope/controller/ConsultorioController.java
@@ -1,13 +1,18 @@
 package com.consultorioapp.ifsertaope.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ConsultorioController {
 	
-	@RequestMapping("/cadastrarPaciente")
+	@RequestMapping("/Inicio")
 	public String form() {
-		return "paciente/formPaciente";
+		return "paciente/Inicio";
+	}
+	@GetMapping("/cadastrarPaciente")
+	public String form1() {
+		return "paciente/formPacientee";
 	}
 }
